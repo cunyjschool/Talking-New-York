b0d4dcc785bbc2f2ae19112edd1521644f8564df
Daniel Bachhuber <danielbachhuber@gmail.com>
2011-04-27 17:18:48 +0000

    Use proper link references and include the footer

--- index.php
+++ index.php
@@ -3,18 +3,18 @@
   <div class="desc"><a href="/about"><?php echo get_bloginfo('description') ?></a></div>
   <div class="video1">
     <div class="spacer1"><object width="250" height="185"><param name="allowfullscreen" value="true" /><param name="allowscriptaccess" value="always" /><param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=11748160&server=vimeo.com&show_title=0&show_byline=0&show_portrait=0&color=&fullscreen=1" /><embed src="http://vimeo.com/moogaloop.swf?clip_id=11748160&server=vimeo.com&show_title=0&show_byline=0&show_portrait=0&color=&fullscreen=1" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="250" height="185"></embed></object></div>
-    <div class="text1"><a href="http://talkingnewyork.com/learning-it/">Learning It</a></div>
+    <div class="text1"><a href="<?php bloginfo('url'); ?>/learning-it/">Learning It</a></div>
   </div>
   <div class="video2">
     <div class="spacer2"><object width="250" height="185"><param name="allowfullscreen" value="true" /><param name="allowscriptaccess" value="always" /><param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=11748145&server=vimeo.com&show_title=0&show_byline=0&show_portrait=0&color=&fullscreen=1" /><embed src="http://vimeo.com/moogaloop.swf?clip_id=11748145&server=vimeo.com&show_title=0&show_byline=0&show_portrait=0&color=&fullscreen=1" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="250" height="185"></embed></object></div>      
-    <div class="text2"><a href="http://talkingnewyork.com/losing-it/">Losing It</a></div>
+    <div class="text2"><a href="<?php bloginfo('url'); ?>/losing-it/">Losing It</a></div>
   </div>
   <div class="video3">
     <div class="spacer3"><object width="250" height="185"><param name="allowfullscreen" value="true" /><param name="allowscriptaccess" value="always" /><param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=11748121&server=vimeo.com&show_title=0&show_byline=0&show_portrait=0&color=&fullscreen=1" /><embed src="http://vimeo.com/moogaloop.swf?clip_id=11748121&server=vimeo.com&show_title=0&show_byline=0&show_portrait=0&color=&fullscreen=1" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="320" height="185"></embed></object></div>
-    <div class="text3"><a href="http://talkingnewyork.com/keeping-it/">Keeping It</a></div>
+    <div class="text3"><a href="<?php bloginfo('url'); ?>/keeping-it/">Keeping It</a></div>
   </div> 
   <div class="video4">
     <div class="spacer4"><object width="250" height="180"><param name="allowfullscreen" value="true" /><param name="allowscriptaccess" value="always" /><param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=11773710&server=vimeo.com&show_title=0&show_byline=0&show_portrait=0&color=&fullscreen=1" /><embed src="http://vimeo.com/moogaloop.swf?clip_id=11773710&server=vimeo.com&show_title=0&show_byline=0&show_portrait=0&color=&fullscreen=1" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="250" height="180"></embed></object></div>
-    <div class="text4"><a href="http://talkingnewyork.com/sharing-it/">Sharing It</a></div>
+    <div class="text4"><a href="<?php bloginfo('url'); ?>/sharing-it/">Sharing It</a></div>
   </div> 
 </div>
